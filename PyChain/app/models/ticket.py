0bea66d23c98f9445659f76a839eed2589162c18
Nathan McNair <nathan.mcnair@example.com>
2025-04-28 15:34:48 -0700

    Restore main branch chain retrieval and fix sw_customfieldvalues query

--- PyChain/app/models/ticket.py
+++ PyChain/app/models/ticket.py
@@ -1,7 +1,6 @@
-from sqlalchemy import Column, Integer, String, SmallInteger, ForeignKey
-from sqlalchemy.orm import relationship
+from sqlalchemy import Column, Integer, String, DateTime, Text
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.sql.sqltypes import Text
+from sqlalchemy.sql import func
 
 Base = declarative_base()
 
@@ -15,18 +14,7 @@ class Ticket(Base):
     resolutiondateline = Column(Integer)  # Unix timestamp for closed
     duedate = Column(Integer)  # Unix timestamp for service_date
     totalreplies = Column(Integer)
-    locationid = Column(Integer)
-    tickettypeid = Column(Integer)
-    departmentid = Column(Integer)
     lastpostid = Column(Integer)
-    posts = relationship("Posts", back_populates="ticket")
-
-class Posts(Base):
-    __tablename__ = 'sw_ticketposts'
-    ticketpostid = Column(Integer, primary_key=True)
-    ticketid = Column(Integer, ForeignKey('sw_tickets.ticketid'))
-    dateline = Column(Integer)  # Unix timestamp
+    tickettypetitle = Column(String(255))
+    departmenttitle = Column(String(255))
     fullname = Column(String(255))
-    contents = Column(Text)
-    isprivate = Column(SmallInteger)
-    ticket = relationship("Ticket", back_populates="posts")
